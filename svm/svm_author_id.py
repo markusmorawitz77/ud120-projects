91329fa7c3b10848d7af7b6a2a7407afc638d281
Markus Morawitz <mora@markuss-mini.home>
2019-01-30 17:17:22 +0100

    final quiz fro svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,13 +25,14 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 from sklearn.svm import SVC
+
 features_train = features_train[:len(features_train)/1] 
 labels_train = labels_train[:len(labels_train)/1] 
 
-for c in [100]:
+for c in [10000]:
     print 
     print "train with C =", c
-    clf = SVC(kernel="rbf", gamma="scale", C=c)
+    clf = SVC(kernel="rbf", gamma="auto", C=c)
     t0 = time()
     clf.fit(features_train, labels_train)
     print "traning time:", round(time()-t0, 3), " s"
@@ -41,6 +42,17 @@ for c in [100]:
     accuracy = clf.score(features_test, labels_test)
     print "test time:", round(time()-t0, 3), " s"
     print "accuracy:", accuracy
+
+    p = clf.predict(features_test)
+    zeros = 0
+    ones = 0
+    for y in p:
+        if y == 1: 
+            ones += 1
+        elif y == 0: 
+            zeros += 1
+    print "0:", zeros
+    print "1:", ones
 
 
 #########################################################

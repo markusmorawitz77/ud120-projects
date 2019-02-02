9013c5276af4d5ac937ccc3e64990bcbccc4ed80
Markus Morawitz <mora@markuss-mini.home>
2019-02-02 18:19:23 +0100

    Testing Adaboost, k nearest neighbor and random forest

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -17,22 +17,35 @@ bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 
 
 #### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
+# plt.xlim(0.0, 1.0)
+# plt.ylim(0.0, 1.0)
+# plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+# plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+# plt.legend()
+# plt.xlabel("bumpiness")
+# plt.ylabel("grade")
+# plt.show()
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn import neighbors
+from sklearn import ensemble
+from sklearn import tree
 
+#clf = neighbors.KNeighborsClassifier(n_neighbors=50)
+#clf = tree.DecisionTreeClassifier(min_samples_split=70)
+#clf = ensemble.AdaBoostClassifier(n_estimators=70)
+clf = ensemble.RandomForestClassifier(min_samples_split=70, n_estimators=70)
 
+clf.fit(features_train, labels_train)
 
+from sklearn import metrics
+predictions = clf.predict(features_test)
+acc = metrics.accuracy_score(labels_test, predictions)
+print "Accuracy:", acc
+print "Number of training examples:", len(features_train)
 
 
 
